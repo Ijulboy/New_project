d8727713d214baece6edf4155603f52eca64bb49
DESKTOP-Q58E3C9\Yulia <Ijulboy@yandex.ru>
2021-02-26 14:36:46 +0300

    Вывела сумму

---
+++ Anj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Anj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=5, n=5;
+            Console.WriteLine(b+a+n);
+            
+        }
+    }
+}
---
+++ QWEr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QWEr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
